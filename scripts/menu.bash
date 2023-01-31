c4d66ef400798e9bff357305f4f781df06d28372
Alberto-Colab <93279091+Alberto-Colab@users.noreply.github.com>
2023-01-31 10:00:20 -0300

    Update menu.bash

--- scripts/menu.bash
+++ scripts/menu.bash
@@ -24,23 +24,25 @@ ColorBlue(){
 menu(){
 echo -ne "
 Moodle GCP - Setup Menu
-$(ColorGreen '1)') Iniciar configurações.
-$(ColorGreen '2)') Fazer a instalação de rede.
-$(ColorGreen '3)') Iniciar Infraestrutura.
-$(ColorGreen '4)') Configurar permissões.
-$(ColorGreen '5)') Instalar GKE-Kubernet Cluster.
-$(ColorGreen '6)') Instalar Cloud SQL - Mysql.
-$(ColorGreen '7)') Instalar FILESTORE - NFS.
-$(ColorGreen '8)') Instalar Artifact Registry e criar Moodle Image.
-$(ColorGreen '9)') Instalação NS.
-$(ColorGreen '10)') Instalação PV.
-$(ColorGreen '11)') Instalação PVC.
-$(ColorGreen '12)') Deploy Moodle With Helm.
-$(ColorGreen '13)') Deploy backend configuration.
-$(ColorGreen '14)') Provision a new certificate managed by Google Cloud.
-$(ColorGreen '15)') Deploy Ingress - Cloud Load Balancer.
-$(ColorGreen '16)') Enable horizontal scale for Pods.
-$(ColorGreen '17)') Configure Redis.
+$(ColorGreen '1)') Edit Variables ENVS.
+$(ColorGreen '2)') Initial Setup.
+$(ColorGreen '3)') Deploy network resources.
+$(ColorGreen '4)') Deploy linux server.
+$(ColorGreen '5)') Setup permissions.
+$(ColorGreen '6)') Deploy GKE-Kubernet Cluster.
+$(ColorGreen '7)') Deploy Cloud SQL - Mysql.
+$(ColorGreen '8)') Deploy FILESTORE - NFS.
+$(ColorGreen '9)') Deploy Artifact Registry and Moodle Image.
+$(ColorGreen '10)') Deploy NameSpace NS .
+$(ColorGreen '11)') Deploy PV.
+$(ColorGreen '12)') Deploy PVC.
+$(ColorGreen '13)') Deploy Moodle With Helm.
+$(ColorGreen '14)') Deploy backend configuration.
+$(ColorGreen '15)') Provision a new certificate managed by Google Cloud.
+$(ColorGreen '16)') Deploy Ingress - Cloud Load Balancer.
+$(ColorGreen '17)') Enable horizontal scale for Pods.
+$(ColorGreen '18)') Configure Redis.
+$(ColorGreen '20)') I'm in a hurry (Do all process together).
 $(ColorGreen '0)') Exit
 $(ColorBlue 'Choose an option:') "
 
@@ -51,7 +53,7 @@ $(ColorBlue 'Choose an option:') "
 	        3) tcp_check ; menu ;;
 	        4) kernel_check ; menu ;;
 	        5) all_checks ; menu ;;
-	        17) redis;;
+	        18) redis;;
 		0) exit 0 ;;
 		*) echo -e $red"Wrong option."$clear; WrongCommand;;
         esac
